f059c3ff756a05fba9296c206a15ca9314676f38
jeantq2 <jcarlosandrade@estudiante.uniajc.edu.co>
2026-02-23 17:41:12 +0000

    ok

---
+++ Clase4/Ejercicio41b.java
@@ -0,0 +1,19 @@
+package Clase4;
+
+import java.util.Scanner;
+
+public class Ejercicio41b {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(Sytem.in);
+        //si la temperatura esta por encima de 100 sube punto de ebullicion
+        System.out.println("Digite la temperatura...");
+        int temp = sc.nextInt();
+        if (temp >= 100){
+            System.out.println("Punto ebullicion");
+         }else{
+            System.out.println("No esta en punto de ebullicion");
+
+        }
+        sc.close();
+    }
+}
