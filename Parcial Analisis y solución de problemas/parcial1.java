2146741a73bb6738cb310b1e3dcd22595f7abb95
jeantq2 <jcarlosandrade@estudiante.uniajc.edu.co>
2026-03-02 14:18:48 +0000

    ko

--- Parcial Analisis y solución de problemas/parcial1.java
+++ Parcial Analisis y solución de problemas/parcial1.java
@@ -1,34 +1,29 @@
-package Parcial Analisis y solución de problemas;
+import java.util.Scanner;
 
 public class parcial1 {
      public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-          final int CLAVE = 1234;
+            try (Scanner sc = new Scanner(System.in)) {
+                final int CLAVE = 1234;
         final int SALDO_INICIAL = 800000;
-            int claveUsuario;
+         int claveUsuario;
         int retiro;
         int nuevoSaldo;
-        // Pedir la clave del usuario
-        System.out.println("Digite su clave: ");
-        claveUsuario = sc.nextInt();
-        if (claveUsuario == CLAVE) {
-            System.out.println("Digite el monto a retirar: ");
-            retiro = sc.nextInt();
-             if (retiro > 200000) {
-                retiro = retiro + 2000; // Se cobra comisión
-            }
-            if (retiro <= SALDO_INICIAL) {
-                nuevoSaldo = SALDO_INICIAL - retiro;
-                System.out.println("Su saldo actual es: " + nuevoSaldo + " Muchas Gracias!!!!");
-             } else {
-                 System.out.println("El valor solicitado más el costo de la transacción es mayor al saldo.");
+
+                System.out.print("Ingrese su clave: ");
+                claveUsuario = sc.nextInt();
+                
+                if (claveUsuario == CLAVE) {
+                    System.out.print("Ingrese el monto a retirar: ");
+                    retiro = sc.nextInt();
+                     retiro = retiro + 2000; // Se cobra comisión
+                    nuevoSaldo = SALDO_INICIAL - retiro;
+                    System.out.println("Nuevo saldo: " + nuevoSaldo);
                 } else {
-                    System.out.println("Su clave no es correcta... intenta nuevamente.");
-                 }
+                    System.out.println("Clave incorrecta");
+                    sc.close();
                 }
-             sc.close();
-
+            }
      }
-
 }
+          
+          
